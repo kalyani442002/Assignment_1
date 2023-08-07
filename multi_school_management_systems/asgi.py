0a57b89197a4a56e0b87c2b2258a9e4993bb5775
rajyalakshmi <rajianna239@gmail.com>
2023-08-07 10:33:25 +0530

    first commit

---
+++ multi_school_management_systems/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for multi_school_management_systems project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "multi_school_management_systems.settings"
+)
+
+application = get_asgi_application()
